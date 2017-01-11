c5632e93993b33b076ea99c3517a77f112ffe1aa
sara-02 <sarahmasud02@gmail.com>
2017-01-11 17:15:33 +0530

    Change memory value

--- gremlin/scripts/entrypoint.sh
+++ gremlin/scripts/entrypoint.sh
@@ -5,6 +5,8 @@ PROPS=${SERVER_DIR}/conf/gremlin-server/dynamodb.properties
 GREMLIN_CONF=${SERVER_DIR}/conf/gremlin-server/gremlin-server.yaml
 GREMLIN_HOST=$HOSTNAME
 RESPONSE_TIMEOUT=100000
+GREMLIN_SERVER_SH=${SERVER_DIR}/bin/gremlin-server.sh
+JVM_VALUE="-Xms1024m -Xmx2048m -javaagent:$LIB/jamm-0.3.0.jar"
 
 sed -i.bckp 's#host: .*#host: '$GREMLIN_HOST'#' ${GREMLIN_CONF}
 sed -i.bckp 's#storage.dynamodb.client.credentials.class-name=.*#storage.dynamodb.client.credentials.class-name='$1'#' ${PROPS}
@@ -18,6 +20,8 @@ fi
 if [ "$REST" == "1" ]; then
     sed -i.bckp 's#channelizer: .*#channelizer: org.apache.tinkerpop.gremlin.server.channel.HttpChannelizer#' ${GREMLIN_CONF}
 fi
+
+sed -i.bckp 's/^ *#*JAVA_OPTIONS="-Xms32m -Xmx512m -javaagent:$LIB/jamm-0.3.0.jar/"'$JVM_VALUE'/' ${GREMLIN_SERVER_SH}
 
 cd ${SERVER_DIR}
 
